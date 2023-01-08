8f74eb5982f5670e1d3d73eb65083105df938f14
Timea-hub <timea.ciceu@gmail.com>
2023-01-08 05:26:12 +0200

    auth

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, ToastController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { LoginPage } from './login.page';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { createAction, Store, StoreModule } from '@ngrx/store';
+import { loadingReducer } from 'src/store/loading/loading.reducers';
+import { loginReducer } from 'src/store/login/login.reducers';
+import { AppState } from 'src/store/AppState';
+import { recoverPassword, recoverPasswordFail, recoverPasswordSuccess } from 'src/store/login/login.actions';
+import { hide, show } from 'src/store/loading/loading.action';
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
   let router: Router;
+  let page;
+  let store: Store<AppState>;
+  let toastController: ToastController;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -16,14 +25,20 @@ describe('LoginPage', () => {
       imports: [
         IonicModule.forRoot(),
         AppRoutingModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        StoreModule.forRoot([]),
+        StoreModule.forFeature("loading", loadingReducer),
+        StoreModule.forFeature("login", loginReducer)
         ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginPage);
     router = TestBed.get(Router);
+    store = TestBed.get(Store);
+    toastController = TestBed.get(ToastController);
+
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    page = fixture.debugElement.nativeElement;
   }));
 
   it('should go to tabs/home page pressing the login button', () => {
@@ -47,5 +62,50 @@ describe('LoginPage', () => {
 
     expect(component.form).not.toBeUndefined();
   });
+
+  it('should recover email/password of forgot email/password', () => {
+    fixture.detectChanges(); //start page
+    component.form.get('email').setValue("timea@email.com"); //user set valid
+    page.querySelector("#recoverPasswordButton").click();
+    store.select('login').subscribe(loginState => {
+      expect(loginState.isRecoveringPassword).toBeTruthy();
+    })
+  })
+
+  it('should show loading when recovering password', () => {
+    
+    fixture.detectChanges(); //start page
+    store.dispatch(recoverPassword());
+    page.querySelector("#recoverPasswordButton").click();
+    store.select('loading').subscribe(loginState => {
+      expect(loginState.show).toBeTruthy(); 
+    })
+  })
+
+  it('should hide loading component and show a success message when the password is recovered', () => {
+    spyOn(toastController, 'create');
+
+    fixture.detectChanges(); //start page
+    store.dispatch(recoverPassword());
+    store.dispatch(recoverPasswordSuccess());
+    store.select('loading').subscribe(loginState => {
+      expect(loginState.show).toBeFalsy(); 
+    })
+
+    expect(toastController.create).toHaveBeenCalledTimes(1);
+  })
+
+  it('should hide loading component and show a error message when the password is not recovered', () => {
+    spyOn(toastController, 'create');
+
+    fixture.detectChanges(); //start page
+    store.dispatch(recoverPassword());
+    store.dispatch(recoverPasswordFail({error: "message"}));
+    store.select('loading').subscribe(loginState => {
+      expect(loginState.show).toBeFalsy();
+    })
+
+    expect(toastController.create).toHaveBeenCalledTimes(1);
+  })
 
 });
